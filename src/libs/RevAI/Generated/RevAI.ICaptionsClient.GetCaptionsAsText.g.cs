81817d46bdff1fde126892e78dfaf3b951755ed9
Konstantin S. <havendv@gmail.com>
2026-08-25 04:29:48 +0400

    feat: Updated OpenAPI spec (#36)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/RevAI/Generated/RevAI.ICaptionsClient.GetCaptionsAsText.g.cs
+++ src/libs/RevAI/Generated/RevAI.ICaptionsClient.GetCaptionsAsText.g.cs
@@ -16,7 +16,7 @@ namespace RevAI
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::RevAI.ApiException"></exception>
-        global::System.Threading.Tasks.Task<byte[]> GetCaptionsAsync(
+        global::System.Threading.Tasks.Task<string> GetCaptionsAsTextAsync(
             string id,
             global::RevAI.GetCaptionsAccept? accept = default,
             int? speakerChannel = default,
@@ -34,25 +34,7 @@ namespace RevAI
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::RevAI.ApiException"></exception>
-        global::System.Threading.Tasks.Task<global::System.IO.Stream> GetCaptionsAsStreamAsync(
-            string id,
-            global::RevAI.GetCaptionsAccept? accept = default,
-            int? speakerChannel = default,
-            global::RevAI.AutoSDKRequestOptions? requestOptions = default,
-            global::System.Threading.CancellationToken cancellationToken = default);
-        /// <summary>
-        /// Get Captions<br/>
-        /// Returns caption output for a transcription job in SRT or VTT format.
-        /// </summary>
-        /// <param name="accept">
-        /// Default Value: application/x-subrip
-        /// </param>
-        /// <param name="speakerChannel"></param>
-        /// <param name="id"></param>
-        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
-        /// <param name="cancellationToken">The token to cancel the operation with</param>
-        /// <exception cref="global::RevAI.ApiException"></exception>
-        global::System.Threading.Tasks.Task<global::RevAI.AutoSDKHttpResponse<byte[]>> GetCaptionsAsResponseAsync(
+        global::System.Threading.Tasks.Task<global::RevAI.AutoSDKHttpResponse<string>> GetCaptionsAsTextAsResponseAsync(
             string id,
             global::RevAI.GetCaptionsAccept? accept = default,
             int? speakerChannel = default,
